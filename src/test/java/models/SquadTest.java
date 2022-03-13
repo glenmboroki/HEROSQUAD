dad25241ee885b698cda4af25a8160b495e58a3e
glen <glen.mboroki@student.moringaschool.com>
2022-03-13 11:08:08 +0300

    test commit

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,6 +1,42 @@
 package models;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Before;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SquadTest {
-  
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void Squad_instantiatesCorrectly_true() {
+        Squad mySquad = new Squad("marvel",  "computer illiteracy");
+        assertEquals(true, mySquad instanceof Squad);
+    }
+    @Test
+    public void returnsAllInstancesOfSquad_true() {
+        Squad fsquad = new Squad("marvel",  "computer illiteracy");
+        Squad squad = new Squad("Endgame",  "sexism");
+        assertEquals(true, Squad.all().contains(fsquad));
+        assertEquals(true, Squad.all().contains(squad));
+    }
+    @Test
+    public void clearSquadFromArray() {
+        Squad firstSquad = new Squad("Endgame",  "sexism");
+        Squad.deleteAll();
+        assertEquals(0, Squad.all().size());
+    }
+    @Test
+    public void addsHeroToSquad_true() {
+        Squad mySquad = new Squad("Endgame", "protect the earth");
+        Hero myHero = new Hero("Captain America", 26, "Super Strong", "girlfriend", "Endgame");
+        mySquad.addHero(myHero);
+        System.out.println(mySquad.getHeroes().size());
+        Assert.assertEquals(true, mySquad.getHeroes().contains(myHero));
+    }
+    public void tearDown() throws Exception {
+    }
 }
